7bfc056bf71f897a67f45853162f8e4f145cdeaa
Bruno Souza <mr.brunosouza@gmail.com>
2021-06-18 20:45:41 -0300

    ajuste no gráfico do dashboard

--- src/components/DashboardItem/index.tsx
+++ src/components/DashboardItem/index.tsx
@@ -29,15 +29,14 @@ const options  = {
     enabled: false,
   },
   xaxis: {
-    type: 'datetime',
     categories: [
-      '2021-03-18T00:00:00.000Z',
-      '2021-03-19T00:00:00.000Z',
-      '2021-03-20T00:00:00.000Z',
-      '2021-03-21T00:00:00.000Z',
-      '2021-03-22T00:00:00.000Z',
-      '2021-03-23T00:00:00.000Z',
-      '2021-03-24T00:00:00.000Z'
+      '18 Mar',
+      '19 Mar',
+      '20 Mar',
+      '21 Mar',
+      '22 Mar',
+      '23 Mar',
+      '24 Mar'
     ],
     axisBorder: {
       color: theme.colors.gray[600]
